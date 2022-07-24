e4b85d98c0a345b9e106823739c8e58bee33e364
Alireza <alireza.binesh.2000@gmail.com>
2022-07-24 22:14:31 +0430

    [FIX] pull request error

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -3,10 +3,11 @@ import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header.component';
 import {SearchBoxModule} from './search-box/search-box.module';
 import {DividerVerticalModule} from '../divider-vertical/divider-vertical.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
     declarations: [HeaderComponent],
-    imports: [CommonModule, SearchBoxModule, DividerVerticalModule],
+    imports: [CommonModule, SearchBoxModule, DividerVerticalModule, RouterModule],
     exports: [HeaderComponent],
 })
 export class HeaderModule {}
