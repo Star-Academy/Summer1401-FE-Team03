791b09a76cfeec6aa14308dd50532cc61da027aa
Alireza <alireza.binesh.2000@gmail.com>
2022-07-24 12:12:54 +0430

    [FIX] change project name to Summer1401-FE-Team03

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
             suppressAll: true, // removes the duplicated traces
         },
         coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/boilera'),
+            dir: require('path').join(__dirname, './coverage/Summer1401-FE-Team03'),
             subdir: '.',
             reporters: [{type: 'html'}, {type: 'text-summary'}],
         },
