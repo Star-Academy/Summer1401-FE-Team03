42402f9bfde4086ba024b231e9d07de651a5316f
amiratax <faraji.ata92@gmail.com>
2022-08-05 23:35:49 +0430

    [FIX] expansion list

--- src/app/pages/search/component/expansion-list/expansion-list.component.ts
+++ src/app/pages/search/component/expansion-list/expansion-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {ExpansionListModel} from '../../models/expantion-list.model';
 
 @Component({
@@ -12,10 +12,32 @@ export class ExpansionListComponent {
 
     @Output() public itemsChange = new EventEmitter<ExpansionListModel[]>();
 
+    @ViewChild('list') public list: ElementRef | null = null;
+
+    public constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
     public isExpanded: boolean = false;
     public searchPhrase: string = '';
 
     public setPlaceholderText(): string {
         return `${this.title} مدنظرت رو جستجو کن `;
+    }
+
+    public expand(): void {
+        this.isExpanded = !this.isExpanded;
+        this.changeDetectorRef.detectChanges();
+
+        const first = this.list?.nativeElement.getBoundingClientRect();
+        this.list?.nativeElement.classList.toggle('flip');
+        const last = this.list?.nativeElement.getBoundingClientRect();
+
+        const deltaX = first.left - last.left;
+        const deltaY = first.top - last.top;
+
+        // prettier-ignore
+        this.list?.nativeElement.animate([
+            {transform: `translate(${deltaX}px, ${deltaY}px`},
+            {transform: `translate(0)`},
+        ]);
     }
 }
