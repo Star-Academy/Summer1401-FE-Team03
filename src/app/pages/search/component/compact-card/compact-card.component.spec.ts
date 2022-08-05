6bdeb05e82e785f344e93b625fbe6281c026b2d9
amiratax <faraji.ata92@gmail.com>
2022-08-05 22:20:42 +0430

    [FIX] spec files

--- src/app/pages/search/component/compact-card/compact-card.component.spec.ts
+++ src/app/pages/search/component/compact-card/compact-card.component.spec.ts
@@ -1,24 +1,3 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-
 import {CompactCardComponent} from './compact-card.component';
 
-describe('CompactCardComponent', () => {
-    let component: CompactCardComponent;
-    let fixture: ComponentFixture<CompactCardComponent>;
-
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [CompactCardComponent],
-        }).compileComponents();
-    });
-
-    beforeEach(() => {
-        fixture = TestBed.createComponent(CompactCardComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
-
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
-});
+describe('CompactCardComponent', () => {});
--- src/app/pages/search/component/expansion-list/expansion-search-box/expansion-search-box.component.spec.ts
+++ src/app/pages/search/component/expansion-list/expansion-search-box/expansion-search-box.component.spec.ts
@@ -1,24 +1,3 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-
 import {ExpansionSearchBoxComponent} from './expansion-search-box.component';
 
-describe('ExpansionSearchBoxComponent', () => {
-    let component: ExpansionSearchBoxComponent;
-    let fixture: ComponentFixture<ExpansionSearchBoxComponent>;
-
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [ExpansionSearchBoxComponent],
-        }).compileComponents();
-    });
-
-    beforeEach(() => {
-        fixture = TestBed.createComponent(ExpansionSearchBoxComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
-
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
-});
+describe('ExpansionSearchBoxComponent', () => {});
