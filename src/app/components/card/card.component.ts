b587e8b2c78900aac7d705867358825b2a60eb68
amiratax <faraji.ata92@gmail.com>
2022-08-13 16:32:05 +0430

    [FIX] minor issues

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {CartService} from '../../services/cart/cart.service';
 import {GameModel} from '../../models/game/game.model';
 
@@ -7,15 +7,10 @@ import {GameModel} from '../../models/game/game.model';
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
     @Input() public game!: GameModel;
 
     public readonly DASH: string = '_';
-    public navigateToGamePage: string = '';
 
     public constructor(public cartService: CartService) {}
-
-    public ngOnInit(): void {
-        this.navigateToGamePage = `/game/${this.game.id}`;
-    }
 }
